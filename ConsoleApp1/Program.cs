d210ab48080e41b40488a0a02915bf9b2e94bed8
Mehmet Sezer <mehmetszr05@gmail.com>
2022-04-24 03:33:51 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] list = new int[15];
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i] = rand.Next(100);
+                Console.WriteLine(i + " = " + list[i]);
+            }
+
+            Console.WriteLine("Aritmetic Mean = " + AritmeticMean(list));
+            Console.ReadLine();
+        }
+
+        public static int AritmeticMean(int[] list)
+        {
+            int sum = 0;
+            int countOfList = list.Length;
+            foreach (int item in list)
+            {
+                sum += item;
+            }
+            return sum / countOfList;
+        }
+    }
+}
